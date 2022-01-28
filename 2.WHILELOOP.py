dd108ba7a26313f16369ed9933eb5a67c05576d6
Shruti Pralhad Chaudhari <84343751+shrutichaudhari4@users.noreply.github.com>
2022-01-28 19:19:54 +0530

    Loop

---
+++ 2.WHILELOOP.py
@@ -0,0 +1,46 @@
+#loop
+#while loop
+#print("hello world")#10 times
+# i=1
+# while i<=10:
+#     print("hello world")
+#     i=i+1
+#print i as long as 1 is less than 6
+# i=1
+# while i<=6:
+#     print(i)
+#     i=i+1
+
+
+
+#sum of numbers
+# n=int(input("enter the numbers"))
+# sum=0
+# i=1
+# while i<=n:
+#     sum+=i
+#     i +=1
+
+# print("the sum is",sum)
+
+
+# number=input("enter the number:")
+# #int(number[i]) +
+# total=0
+# i=0
+# while i<len(number):
+#     total+=int(number[i])
+#     i+=1
+# print(total)    
+
+# a="shruti chudhari"
+# b=""
+# i=0
+# while i<len(a):
+#     if a[i] not in b:
+#         print(f"{a[i]}:{a.count(a[1])}")
+#         i+=1
+
+
+# a="abcxyz"
+# print(a.replace("xyz","abc"))
