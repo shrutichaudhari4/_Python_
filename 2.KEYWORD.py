ff27d8806e473c70666a792b539186ccb12a9546
Shruti Pralhad Chaudhari <84343751+shrutichaudhari4@users.noreply.github.com>
2022-01-28 19:16:16 +0530

    and ,  or Operator And In Keyword

---
+++ 2.KEYWORD.py
@@ -0,0 +1,27 @@
+#and, or operators
+name="abc"
+age=19
+if name =="abc" and age==19:
+    print("condition is true")
+else:
+    print("condition is false")
+    
+    
+ # in keyword
+# if with in 
+name="shruti" 
+if 'h' in name:
+    print("h is present in name")
+else:
+    print("not present")      
+
+#check empty or not
+#important
+
+name=input("enter ypur name")
+if name:
+    print(f"your name is {name}")
+else:
+    print("you didn't type anything")      
+    
+    
