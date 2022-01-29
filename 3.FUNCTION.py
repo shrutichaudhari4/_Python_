46ea17261f28c708f649a823bb9c20c7048534f2
Shruti Pralhad Chaudhari <84343751+shrutichaudhari4@users.noreply.github.com>
2022-01-29 21:22:15 +0530

    function

---
+++ 3.FUNCTION.py
@@ -0,0 +1,28 @@
+#function
+def add_two(a,b):
+    return a+b
+total=add_two(5,6)
+print(total)    
+
+
+def sub_two(a,b):
+    return a-b
+c=sub_two(6,1)
+print(c)    
+
+def myfile(name):
+    return name[-1]
+print(myfile("shruti"))      
+
+def even_odd(num):
+    if num%2==0:
+        return "even"
+    else:
+        return "odd"    
+print(even_odd(9))        
+oRR
+def even_odd(num):
+    if num%2==0:
+        return "even"
+    return "odd"
+print(even_odd(9))    
