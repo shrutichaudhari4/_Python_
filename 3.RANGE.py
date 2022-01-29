1f98655768eb813285a142f928724dfe8f9c4b28
Shruti Pralhad Chaudhari <84343751+shrutichaudhari4@users.noreply.github.com>
2022-01-29 21:20:28 +0530

    Range

---
+++ 3.RANGE.py
@@ -0,0 +1,46 @@
+# a="shruti"
+# b=""
+# i=0
+# while i < len(a):
+#     if a[i] not in b:
+#         b+=a[i]
+#         print(f"{a[i]}:{a.count(a[i])}")
+#         i+=1
+
+
+#sum from 1 to 10
+# sum=0
+# for i in range(1,11):
+#     sum +=i
+#     print(sum)
+
+# num=int(input("enter the num"))
+# sum=0
+# for i in range(0,num):
+#     sum=sum+i
+#     print(sum)
+
+# num=input("enter the num")
+# sum=0
+# for i in range(0,len(num)):
+#         sum+=int(num[i])
+# print(sum)          
+
+# a=input("enter the name")
+# b=""
+# for i in range(1,len(a)):
+#     if a[i] not in b:
+#         print(f"{a[i]}:{a.count(a[i])}")
+#         b+=a[i]
+
+# for i in range(0,11):
+#     if i==6:
+#         break
+#     print(i)
+
+# for i in range(0,11):
+#     if i==5:
+#         continue
+#     print(i)
+
+
